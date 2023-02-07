d4ae7d872766ef86626b950cb21c75c7c561b6b4
PappaBaloo <thebettercharlie@gmail.com>
2023-02-07 10:11:02 +0100

    fonts, main menu

--- main.c
+++ main.c
@@ -8,6 +8,9 @@
 #include <float.h>
 
 long oldseed;
+Color neonBlue = (Color){135, 206, 250, 255};
+Color neonPurple = (Color){255, 0, 255, 255};
+Color neonRed = (Color){255, 0, 128, 255};
 
 int getRand(int min, int max)
 {
@@ -35,6 +38,9 @@ int main(void)
     const int screenHeight = 450;
     InitWindow(screenWidth, screenHeight, "Main Menu");
 
+    // Load custom font
+    Font font = LoadFont("fonts/Alien-Encounters-Italic.ttf");
+
     // Define the main menu options
     const char *options[3] = {
         "New Game",
@@ -42,13 +48,18 @@ int main(void)
         "Exit"};
     int currentOption = 0;
 
-    // Define the font and the font size
-    Font font = GetFontDefault();
-    int fontSize = 20;
+    // Define the font size
+    int fontSize = 40;
+
+    // Define timer
+    float timer = 0.0f;
+    float duration = 2.0f;
 
     // Main game loop
+    SetTargetFPS(60);
     while (!WindowShouldClose())
     {
+
         // Check for user input
         if (IsKeyPressed(KEY_DOWN) || IsKeyPressed(KEY_S))
         {
@@ -76,27 +87,30 @@ int main(void)
             }
         }
 
-        // Clear the screen to a light blue color
+        // Clear the screen
         BeginDrawing();
-        ClearBackground(RAYWHITE);
+        ClearBackground(BLACK);
 
-        // Draw the main menu options
+        // Draw the main menu options and game name
+
+        DrawTextEx(font, "Midnight Miami Mayhem", (Vector2){screenWidth / 2 - MeasureTextEx(font, "Midnight Miami Mayhem", fontSize, 2).x / 2, screenHeight / 2 - fontSize * 3.5f}, fontSize, 2, neonRed);
+
         for (int i = 0; i < 3; i++)
         {
             if (i == currentOption)
             {
-                DrawText(options[i], screenWidth / 2 - MeasureText(options[i], fontSize) / 2, screenHeight / 2 - fontSize * 1.5f + i * fontSize * 2, fontSize, BLACK);
+                DrawTextEx(font, options[i], (Vector2){screenWidth / 2 - MeasureTextEx(font, options[i], fontSize, 2).x / 2, screenHeight / 2 - fontSize * 1.5f + i * fontSize * 2}, fontSize, 2, neonPurple);
             }
             else
             {
-                DrawText(options[i], screenWidth / 2 - MeasureText(options[i], fontSize) / 2, screenHeight / 2 - fontSize * 1.5f + i * fontSize * 2, fontSize, GRAY);
+                DrawTextEx(font, options[i], (Vector2){screenWidth / 2 - MeasureTextEx(font, options[i], fontSize, 2).x / 2, screenHeight / 2 - fontSize * 1.5f + i * fontSize * 2}, fontSize, 2, neonBlue);
             }
         }
-
         EndDrawing();
     }
 
     // Close the window and de-initialize the game context
+    UnloadFont(font);
     CloseWindow();
     return 0;
 }
