6a6bdf19d8171eb225594bf2055a345e2d9321a9
PappaBaloo <thebettercharlie@gmail.com>
2023-02-20 14:44:57 +0100

    options menu, fullscreen functionality (needs patching),

--- main.c
+++ main.c
@@ -9,15 +9,47 @@
 
 // DEV MODE
 #define DEV_MODE
+#define MIN_VOLUME 0
+#define MAX_VOLUME 10
+#define VOLUME_STEP 1
 
 long oldseed;
 Color neonBlue = (Color){135, 206, 250, 255};
 Color neonPurple = (Color){255, 0, 255, 255};
 Color neonRed = (Color){255, 0, 128, 255};
 
+int getRand(int min, int max)
+{
+    time_t seed;
+    seed = time(NULL) + oldseed;
+
+    oldseed = seed;
+
+    srand(seed);
+
+    int n = max - min + 1;
+    int i = rand() % n;
+
+    if (i < 0)
+    {
+        i = -i;
+    }
+    return min + i;
+}
+
 void drawBackgroundLevel(Texture background)
 {
     DrawTexture(background, 0, 0, WHITE);
+}
+
+void drawLevel1(Texture background)
+{
+
+    DrawTexture(background, 0, 0, WHITE);
+}
+
+void placeRectangleRec()
+{
 }
 
 void getControls(Rectangle *player, Camera2D *characterCamera)
@@ -42,25 +74,6 @@ void getControls(Rectangle *player, Camera2D *characterCamera)
     characterCamera->target = (Vector2){player->x, player->y};
 }
 
-int getRand(int min, int max)
-{
-    time_t seed;
-    seed = time(NULL) + oldseed;
-
-    oldseed = seed;
-
-    srand(seed);
-
-    int n = max - min + 1;
-    int i = rand() % n;
-
-    if (i < 0)
-    {
-        i = -i;
-    }
-    return min + i;
-}
-
 int main(void)
 {
     // Initialize the window and the game context
@@ -70,6 +83,13 @@ int main(void)
 
     // Load custom font
     Font font = LoadFont("fonts/Alien-Encounters-Italic.ttf");
+
+    // Variables to store fullscreen and volume level
+    bool fullscreen = false;
+    int volume = 5;
+
+    // Load Crosshair textures and disable default mouse cursor
+    Texture2D crosshair = LoadTexture("textures(crosshairs.png)");
 
     // Load level textures
     Texture2D level1 = LoadTexture("textures/level1.png");
@@ -81,6 +101,13 @@ int main(void)
         "Exit"};
     int currentOption = 0;
 
+    // Define the options menu options
+    const char *optionsOptions[3] = {
+        "Fullscreen",
+        "Volume",
+        "Back"};
+    int currentOptioneOption = 1;
+
     // Define the font size
     int fontSize = 40;
 
@@ -89,7 +116,7 @@ int main(void)
     float duration = 2.0f;
 
     // Define level 1 start
-    Vector2 levelStartPos = {30, 30};
+    Vector2 levelStartPos = {30, level1.height - 30};
 
     // Define player rec in temporary replacement for the texture
     Rectangle player;
@@ -108,6 +135,9 @@ int main(void)
     SetTargetFPS(60);
     while (!WindowShouldClose())
     {
+
+        // Define mouse position and texture
+        Vector2 mousePos = GetMousePosition();
 
         // Check for user input
         if (IsKeyPressed(KEY_DOWN) || IsKeyPressed(KEY_S))
@@ -131,7 +161,7 @@ int main(void)
 
                     // Player movement and camera controls
                     getControls(&player, &characterCamera);
-                    characterCamera.zoom += ((float)GetMouseWheelMove() * 0.05f);
+                    // characterCamera.zoom += ((float)GetMouseWheelMove() * 0.05f);
                     characterCamera.zoom = Clamp(characterCamera.zoom, 0.5f, 2.0f);
 
                     // Clear the screen
@@ -141,6 +171,9 @@ int main(void)
                     {
                         DrawTexture(level1, 0, 0, WHITE);
                         DrawRectangleRec(player, WHITE);
+                        DrawTexture(crosshair, mousePos.x, mousePos.y, WHITE);
+                        printf("MousePosX: %f \n", mousePos.x);
+                        printf("MousePosY: %f", mousePos.y);
                     }
 
                     EndDrawing();
@@ -150,6 +183,52 @@ int main(void)
                 break;
             case 1:
                 // Open the options menu
+                while (!WindowShouldClose()) // Detect window close button or ESC key
+                {
+                    if (IsKeyPressed(KEY_DOWN) || IsKeyPressed(KEY_S))
+                    {
+                        currentOptioneOption = (currentOptioneOption + 1) % 3;
+                    }
+                    else if (IsKeyPressed(KEY_UP) || IsKeyPressed(KEY_W))
+                    {
+                        currentOptioneOption = (currentOptioneOption + 2) % 3;
+                    }
+                    else if (IsKeyPressed(KEY_ENTER) || IsKeyPressed(KEY_SPACE))
+                    {
+                        switch (currentOptioneOption)
+                        {
+                        case 0:
+
+                            ToggleFullscreen();
+                            fullscreen = true; // Fix later
+
+                            break;
+                        case 1:
+                            break;
+                        case 2:
+                            break;
+                        }
+                    }
+
+                    // Clear screen
+                    BeginDrawing();
+                    ClearBackground(BLACK);
+
+                    for (int i = 0; i < 3; i++)
+                    {
+                        if (i == currentOptioneOption)
+                        {
+                            DrawTextEx(font, optionsOptions[i], (Vector2){screenWidth / 2 - MeasureTextEx(font, optionsOptions[i], fontSize, 2).x / 2, screenHeight / 2 - fontSize * 1.5f + i * fontSize * 2}, fontSize, 2, neonPurple);
+                        }
+                        else
+                        {
+                            DrawTextEx(font, optionsOptions[i], (Vector2){screenWidth / 2 - MeasureTextEx(font, optionsOptions[i], fontSize, 2).x / 2, screenHeight / 2 - fontSize * 1.5f + i * fontSize * 2}, fontSize, 2, neonBlue);
+                        }
+                    }
+
+                    // End drawing
+                    EndDrawing();
+                }
                 break;
             case 2:
                 // Exit the game
@@ -183,6 +262,7 @@ int main(void)
 
     // Close the window and de-initialize the game context
     UnloadTexture(level1);
+    UnloadTexture(crosshair);
     UnloadFont(font);
     CloseWindow();
     return 0;
